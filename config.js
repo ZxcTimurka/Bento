5268feea47e22814563100909bd818f3c9a21789
ZxcTimurka <130562086+ZxcTimurka@users.noreply.github.com>
2023-11-08 21:38:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -32,8 +32,8 @@ const CONFIG = {
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
 	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-	defaultLatitude: '37.775',
-	defaultLongitude: '-122.419',
+	defaultLatitude: '56.501041',
+	defaultLongitude: '84.992455',
 
 	// Autochange
 	autoChangeTheme: true,
