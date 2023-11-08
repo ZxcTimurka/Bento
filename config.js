af35c547cadd7195a260f2d76531cd974f780bca
ZxcTimurka <130562086+ZxcTimurka@users.noreply.github.com>
2023-11-08 21:27:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -161,12 +161,12 @@ const CONFIG = {
 			id: '2',
 			links: [
 				{
-					name: 'Linkedin',
-					link: 'https://www.linkedin.com',
+					name: 'RuTracker',
+					link: 'https://rutracker.org/forum/index.php',
 				},
 				{
-					name: 'Dribbble',
-					link: 'https://www.dribbble.com',
+					name: 'TIgruha',
+					link: 'https://itorrents-igruha.org/',
 				},
 				{
 					name: 'Trello',
